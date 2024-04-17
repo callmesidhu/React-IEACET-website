cb3dc0eb5c9b9e2ddc4bfbcd0f678ab23d3ecf8d
callmesidhu <sidharthpunalur@gmail.com>
2024-04-18 00:26:43 +0530

    Vicitors analytics added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
