37d4813c0a25c788cb95ecb4c13720281b0095c1
callmesidhu <sidharthpunalur@gmail.com>
2024-04-28 03:55:03 +0530

    navbar made reponsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
 import logo from '../Images/cet-logo.jpg';
 
-function Header({setState}) {
-  
+function Header({ setState }) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <div>
-      <div class="header col-sm-12 col-md-12 col-lg-12">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand p-3 px-4" href="#"><img class="logo" src={logo}></img><b>IEA-CET</b></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active text-white" onClick={()=>{setState("Home")}}>
-          <a class="nav-link " id="nav-link-text" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.nexeracet.tech/" target="_blank">Fest</a>
-        </li>
-        <li class="nav-item" onClick={()=>{setState("Team")}}>
-          <a class="nav-link " href="#Team">Team</a>
-        </li>
-        <li class="nav-item" onClick={()=>{setState("Contact")}}>
-          <a class="nav-link" href="#Contact">Contact</a>
-        </li>
-        <li class="nav-item" onClick={()=>setState("About")}>
-          <a class="nav-link " href="#About">About</a>
-        </li>
-      </ul>
+      <div className="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand p-3 px-4" href="#"><img className="logo" src={logo} alt="IEA-CET Logo" /><b>IEA-CET</b></a>
+          <button className={`navbar-toggler ${isNavOpen ? 'collapsed' : ''}`} type="button" onClick={toggleNav} aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`} id="navbarNav">
+            <ul className="navbar-nav ml-5">
+              <li className="nav-item active text-white" onClick={() => setState("Home")}>
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.nexeracet.tech/" target="_blank" rel="noopener noreferrer">Fest</a>
+              </li>
+              <li className="nav-item" onClick={() => setState("Team")}>
+                <a className="nav-link" href="#Team">Team</a>
+              </li>
+              <li className="nav-item" onClick={() => setState("Contact")}>
+                <a className="nav-link" href="#Contact">Contact</a>
+              </li>
+              <li className="nav-item" onClick={() => setState("About")}>
+                <a className="nav-link" href="#About">About</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <hr className="line"></hr>
     </div>
-  </nav>
-  </div>
-  <hr class="line"></hr>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
