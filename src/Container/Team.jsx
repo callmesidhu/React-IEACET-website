d78b948488680c45356b76afcac6fb4de581f934
callmesidhu <sidharthpunalur@gmail.com>
2024-04-29 23:02:07 +0530

    team section updated

--- src/Container/Team.jsx
+++ src/Container/Team.jsx
@@ -5,7 +5,9 @@ import WebHead from '../Team/WebHead.jpg'
 function Team() {
   return (
     <div class="my-5 py-2">
+      
     <div class="row p-3 m-5 justify-content-around team-bg py-5">
+    <h1 class='display-3 card-head acada text-center team-text mb-5'><b>Team</b></h1>
       <Card style={{ width: '18rem' }} class="p-1 m-2">
       <Card.Body>
       <Card.Img variant="top" src={WebHead} />
