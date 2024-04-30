fd55c381687fe02e71b8291e3e968f9d3389523f
callmesidhu <sidharthpunalur@gmail.com>
2024-04-30 22:47:24 +0530

    Contact Content added

--- src/Container/About.jsx
+++ src/Container/About.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
+
 function About() {
   return (
-    <div class="my-5 py-2">
-      
-    <div class="row p-3 m-5 justify-content-around team-bg py-5">
-    <h1 class='display-3 card-head acada text-center team-text mb-5'><b>About</b></h1>
-    
-      
-    </div>
-     </div>
+<div>
+  
+</div>
   )
 }
-
 export default About
--- src/Container/Contact.jsx
+++ src/Container/Contact.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import '../Container/Container.css'
+import '../Container/Contact.css'
 
 
 function Contact() {
   return (
-    <div class="my-5 py-2">  
+    <div class="my-5 py-2 justify-content-around">  
     <div class="container">
   <div class="box">
     <span></span>
     <div class="content">
       <h2>Contact</h2>
-      <p>Sidharth : 9496225620 <br></br> Sundar Pichai : 90485</p>
-      
+      <p><b> Sayan&nbsp; &nbsp; &nbsp;: 7736864964  <br></br> Devan&nbsp; &nbsp; &nbsp;: 9495477301<br></br> Sidharth : 9496225620</b></p>
     </div>
   </div>
 </div>
