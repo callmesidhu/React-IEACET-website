6a32b063e3f223da70746b35406cf1f5707c577e
callmesidhu <sidharthpunalur@gmail.com>
2024-05-04 02:56:42 +0530

    Team Subcard added

--- src/Container/Team.jsx
+++ src/Container/Team.jsx
@@ -3,9 +3,8 @@ import Card from 'react-bootstrap/Card';
 import WebHead from '../Team/WebHead.jpg'
 import WebTeam1 from '../Team/WebTeam1.jpg'
 import WebTeam2 from '../Team/WebTeam2.jpg'
-import ContentTeam from './ContentTeam'
-import PYQ from './PYQTeam';
-import Notes from './NotesTeam'
+import PYQTeam from './PYQTeam';
+import NotesTeam from './NotesTeam';
 
 function Team() {
   
@@ -15,44 +14,49 @@ function Team() {
     marginRight: '3%',
     marginBottom: '50px'
   }
+  
   return (
     <div class="my-5 py-2">
-      
-    <div class="row justify-content-around team-bg py-5 rounded-sm-md-lg"   style={{margin:'5%'}}>
-    <h1 class='display-3 card-head acada text-center team-text mb-5'><b>Team</b></h1>
-    
-      <Card style={teamImage} class="p-1 ">
-      <Card.Body>
-      <Card.Img variant="top" src={WebHead} />
-      <h3 class="m-2 text-center team-text"><b>S Sidharth</b></h3>
-      <h4 class="text-center team-text">Web Team - Head</h4>
-      </Card.Body>
-      </Card>
+      <div class="row justify-content-around team-bg py-5 rounded-sm-md-lg" style={{ margin: '5%' }}>
+        <h1 class='display-3 card-head acada text-center team-text mb-5'><b>Team</b></h1>
+        
+        <Card style={teamImage} class="p-1 ">
+          <Card.Body>
+            <Card.Img variant="top" src={WebHead} />
+            <h3 class="m-2 text-center team-text"><b>S Sidharth</b></h3>
+            <h4 class="text-center team-text">Web Team - Head</h4>
+          </Card.Body>
+        </Card>
 
-      <Card style={teamImage} class="p-1">
-      <Card.Body>
-      <Card.Img variant="top" src={WebTeam1} />
-      <h3 class="m-2 text-center team-text"><b>Nooh A Vajid</b></h3>
-      <h4 class="text-center team-text">Web Team</h4>
-      </Card.Body>
-      </Card>
+        <Card style={teamImage} class="p-1">
+          <Card.Body>
+            <Card.Img variant="top" src={WebTeam1} />
+            <h3 class="m-2 text-center team-text"><b>Nooh A Vajid</b></h3>
+            <h4 class="text-center team-text">Web Team</h4>
+          </Card.Body>
+        </Card>
 
-      <Card style={teamImage} class="p-1">
-      <Card.Body>
-      <Card.Img variant="top" src={WebTeam2} />
-      <h3 class="m-2 text-center team-text"><b>Nived C</b></h3>
-      <h4 class="text-center team-text">Web Team</h4>
-      </Card.Body>
-      </Card>
+        <Card style={teamImage} class="p-1">
+          <Card.Body>
+            <Card.Img variant="top" src={WebTeam2} />
+            <h3 class="m-2 text-center team-text"><b>Nived C</b></h3>
+            <h4 class="text-center team-text">Web Team</h4>
+          </Card.Body>
+        </Card>
 
-      
-     <ContentTeam/>
-      
+        <div className='row justify-content-around'>
+        <div className='col-lg-6'>
+          <PYQTeam />
+        </div>
+        <div className='col-lg-6'>
+          <NotesTeam />
+        </div>
+      </div>
+      </div>
 
-
+     
     </div>
-     </div>
   )
 }
 
-export default Team
+export default Team;
